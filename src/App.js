a72f89b1eaf40798a084a3064bf307a34dfa51e8
RavalVarun <44644972+RavalVarun@users.noreply.github.com>
2020-05-13 13:32:27 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+class Contactlist extends React.Component {
+  render() {
+    const people = this.props.contacts
+
+    return <ol>
+      {people.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contactlist contacts = {[
+        {name: 'Tyler'},
+        {name: 'Karen'},
+        {name: 'Richard'}
+      ]}/>
+
+      <Contactlist contacts = {[
+        {name: 'Amanda'},
+        {name: 'Mikenzi'},
+        {name: 'Ryan'}
+      ]}/>
     </div>
   );
 }
